51fcaca634a80641acb06526a6a792c0b5839fd0
Daniela Méndez <daniela.mdz.hdz@gmail.com>
2025-11-15 14:22:53 -0600

    2do commint

---
+++ contacto.php
@@ -0,0 +1,10 @@
+<?php require_once 'includes/header.php'; ?>
+
+  <div class="container py-5">
+    <h1 class="text-center">Información de Contacto</h1>
+    <p>Puedes contactarnos usando la información del pié de página o a través de este (futuro) formulario.</p>
+    <p>Email: waggy@gmail.com</p>
+    <p>Teléfono: +980-34984089</p>
+  </div>
+
+<?php require_once 'includes/footer.php'; ?>
---
+++ includes/db_connect.php
@@ -0,0 +1,23 @@
+<?php
+// Archivo: includes/db_connect.php
+// ¡CONFIGURACIÓN PARA XAMPP!
+
+$servername = "localhost";     // O '127.0.0.1'
+$username = "root";            // Usuario por defecto de XAMPP
+$password = "";                 // La contraseña por defecto de XAMPP es VACÍA
+$dbname = "tienda_online";     // El nombre de tu base de datos
+
+// --- NO TOQUES NADA DEBAJO DE ESTA LÍNEA ---
+
+// Crear la conexión
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if(mysqli_connect_errno()) {
+    die("Error de conexión a la Base de Datos: " . mysqli_connect_error());
+}
+
+// Asegurarnos de que hable en UTF-8 (para acentos y ñ)
+mysqli_set_charset($conn, "utf8mb4");
+
+?>  
